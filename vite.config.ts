4cb6e7e0e49ace02a64824218d9ed7d9fb523dc7
Hemant Kumar <hemant.kumar@crowdbotics.com>
2023-09-26 23:14:11 +0530

    Vite + React Proxy Setup for development phase

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // proxy setup as per https://levelup.gitconnected.com/exploring-vite-fast-development-and-proxy-server-setup-for-effortless-frontend-development-d5e654e408e0
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fletcher-40835-staging.botics.co',
+        changeOrigin: true
+      }
+    }
+  }
 })
